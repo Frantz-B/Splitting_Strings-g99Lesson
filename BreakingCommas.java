59b6864e99cb5dec35673026685e1c3e03813cd4
Frantz-B <Frantz.lesly@gmail.com>
2015-06-24 08:58:25 -0400

    Adding snippet to split only part of the string

--- BreakingCommas.java
+++ BreakingCommas.java
@@ -3,9 +3,14 @@ class BreakingCommas{
 	public static void main(String[] args){
 		//feel like putting the below in an array already however I can see the use if an User was entering this data.
 		String teams = "Alpha Team, Bravo Team, Delta Team, Gamma Team, Sigma Team";
-		String[] omegaSplit = teams.split(",");//tried on purpose w/o putting an argument & was not allowed
+		String[] omegaSplit = teams.split(", ");//knew the print would have a space before the text til I made the change
 		for (int i=0; i<omegaSplit.length; i++){
 			System.out.println(omegaSplit[i]);
+		}
+		String[] topTeams = teams.split(", ", 4);
+		System.out.println("\n\n\n");
+		for (int i=0; i<omegaSplit.length; i++){
+			System.out.println("The Top Team(s) with a High Succes"+omegaSplit[i]);
 		}
 	}
 }
