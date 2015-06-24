ca63ca60884530bfe553a0e2af24b9b3c31ae790
Frantz-B <Frantz.lesly@gmail.com>
2015-06-24 09:10:26 -0400

    Adding snippet to split only part of the string..Fixed

--- BreakingCommas.java
+++ BreakingCommas.java
@@ -7,10 +7,11 @@ class BreakingCommas{
 		for (int i=0; i<omegaSplit.length; i++){
 			System.out.println(omegaSplit[i]);
 		}
-		String[] topTeams = teams.split(", ", 4);
+		String[] topTeams = teams.split(", ", 4);//modified this from omegaSplit to topTeam, but initially got an error.
+		//arrayOutOfBounds exception when it was 4, but understand why now.  For loop must have one of the arrays w/ on 3 things
 		System.out.println("\n\n\n");
-		for (int i=0; i<omegaSplit.length; i++){
-			System.out.println("The Top Team(s) with a High Succes"+omegaSplit[i]);
+		for (int i=0; i<topTeams.length; i++){
+			System.out.println("The Top Team(s) with a High Succes : "+topTeams[i]);
 		}
 	}
 }
